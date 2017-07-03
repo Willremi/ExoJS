21859eebf6e8f5e1ab5c01371c66169f78342171
popschool <popschool@debian>
2017-07-03 13:33:43 +0200

    update exo8

--- 3-07-2017/exo8/js/script.js
+++ 3-07-2017/exo8/js/script.js
@@ -1,1 +1,5 @@
-var myName;
+var printForm = function() {
+  var box = document.getElementById("box");
+  box.innerHTML =
+  "Nom: " + document.getElementById("lastname").value + "<br>" + "Prénom: " + document.getElementById("firstname").value + "<br>" + "Age: " + document.getElementById("age").value + "<br>" + "Adresse: " + document.getElementById("addr").value + "<br>";
+}
