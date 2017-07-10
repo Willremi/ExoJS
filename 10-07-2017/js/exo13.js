7538f69248352ef12301ee667cbf22ce6765b006
popschool <popschool@debian>
2017-07-10 11:16:55 +0200

    add exo13

---
+++ 10-07-2017/js/exo13.js
@@ -0,0 +1,27 @@
+var allGreen = function() {
+
+  r2g();
+  b2g();
+}
+
+var r2g = function() {
+				var div = document.getElementsByClassName('red');
+				for (i=0; i<div.length; i++) {
+					div[i].style = 'background-color: green;';
+				}
+			}
+
+			var b2g = function() {
+				var div = document.getElementsByClassName('blue');
+				for (i=0; i<div.length; i++) {
+					div[i].style = 'background-color: green;';
+
+				}
+			}
+
+			var reset = function() {
+				var div = document.getElementsByTagName('div');
+				for (i=0; i<div.length; i++) {
+					div[i].style = '';
+				}
+			}
