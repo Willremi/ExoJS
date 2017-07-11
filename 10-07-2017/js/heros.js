6f9b6b7a2961f67aba2499e2cf1f23ce7df6231c
popschool <popschool@debian>
2017-07-11 16:08:29 +0200

    exoHero working

---
+++ 10-07-2017/js/heros.js
@@ -0,0 +1,10 @@
+var pointPV = function(agility, force, endurance) {
+  return parseInt(agility) + parseInt(force) + parseInt(endurance);
+}
+
+if (pointPV == 100) {
+ //afficher le héros
+}
+else {
+  // afficher "recommencer"
+}
