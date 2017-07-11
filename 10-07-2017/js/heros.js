2feef68c4ad1bf23d18f82157291262081baf905
popschool <popschool@debian>
2017-07-11 17:14:51 +0200

    heros working1

--- 10-07-2017/js/heros.js
+++ 10-07-2017/js/heros.js
@@ -1,10 +1,5 @@
+
+
 var pointPV = function(agility, force, endurance) {
   return parseInt(agility) + parseInt(force) + parseInt(endurance);
-}
-
-if (pointPV == 100) {
- //afficher le héros
-}
-else {
-  // afficher "recommencer"
 }
