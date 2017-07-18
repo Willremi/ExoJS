6725bab26a57e97403a802e1d0f42b19d9cefbd1
popschool <popschool@debian>
2017-07-18 16:55:50 +0200

    update boucle

--- 18-07-2017/js/boucle.js
+++ 18-07-2017/js/boucle.js
@@ -12,3 +12,40 @@ for (var i = 0; i < myArray.length; i++) {
 for(var i = myArray.length - 1; i >= 0; i--) {
   console.log(myArray[i]);
 }
+
+//boucle for in
+for (var i in myArray) {
+  console.log(myArray[i]);
+}
+
+//boucle for of
+for (var value of myArray) {
+  console.log(value);
+}
+
+//afficher les nombres compris entre 0 à 50
+for (var i in myArray) {
+  if(myArray[i] > 0 && myArray[i] < 50) {
+    console.log(myArray[i]);
+  }
+}
+//afficher les nombres supérieurs de 50
+for (var i in myArray) {
+  if(myArray[i] > 50) {
+    console.log(myArray[i]);
+  }
+}
+
+//afficher les nombres décimaux
+for (var value of myArray) {
+  if (!Number.isInteger(value)) {
+    console.log(value);
+  }
+}
+
+//afficher les nombres décimaux (méthode alternative)
+for (var value of myArray) {
+  if (value - Math.round(value) != 0) {
+    console.log(value);
+  }
+}
