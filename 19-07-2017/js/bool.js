dbfe82881fae30594b1097080bf19ee9f24eda43
popschool <popschool@debian>
2017-07-19 10:55:43 +0200

    exo 2 finished in bool.html

--- 19-07-2017/js/bool.js
+++ 19-07-2017/js/bool.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var stock = 100;
+var stock = prompt("Combien vous voulez ?");
 
 // afficher dans la console :
 //-"stock indisponible" si le stock est à zéro
@@ -13,4 +13,23 @@ if (stock == 0) {
   console.log("Stock disponible");
 } else {
   console.log("Stock en faible quantité");
+}
+
+var hasValidAdress = prompt("Adresse validée (true/false)");
+var hasValidatedAgreement = prompt("Condition de vente validée (true/false)");
+
+// afficher dans la console :
+// -"commande en cours" si hasValidAdress et hasValidatedAgreement sont vrais
+// -"vous devez renseigner une adresse" si hasValidAdress est faux
+// -"vous devez cocher la case Condition de vente" si hasValidatedAgreement est faux
+// -"Vous devez renseigner une adresse et cocher la case Condition de vente" si hasValidAdress et hasValidatedAgreement sont faux
+
+if (hasValidAdress == true && hasValidatedAgreement == true) {
+  console.log("Commande en cours");
+} else if (hasValidAdress == false && hasValidatedAgreement == true) {
+  console.log("Vous devez renseigner une adresse");
+} else if (hasValidAdress == true && hasValidatedAgreement == false) {
+  console.log("Vous devez cocher la case Condition de vente");
+} else {
+  console.log("Vous devez renseigner une adresse et cocher la case Condition de vente");
 }
