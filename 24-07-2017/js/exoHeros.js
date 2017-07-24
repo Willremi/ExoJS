8a550126bc41fdeb74e6781eb431ddd971ab16d2
popschool <popschool@debian>
2017-07-24 15:03:41 +0200

    add agility, force, endurance

--- 24-07-2017/js/exoHeros.js
+++ 24-07-2017/js/exoHeros.js
@@ -1,1 +1,11 @@
 "use strict";
+
+function result() {
+  var name = document.getElementById("name").value;
+  var agility = document.getElementById("agility").value;
+  var force = document.getElementById("force").value;
+  var endurance = document.getElementById("endurance").value;
+  var total = parseInt(agility) + parseInt(force) + parseInt(endurance);
+
+  document.getElementById("total").innerHTML = total;
+}
