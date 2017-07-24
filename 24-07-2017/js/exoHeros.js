49bf58a82542dddcb8574d39d1ec33003d5ae3d2
popschool <popschool@debian>
2017-07-24 15:39:39 +0200

    add condition dans exoHeros

--- 24-07-2017/js/exoHeros.js
+++ 24-07-2017/js/exoHeros.js
@@ -8,4 +8,9 @@ function result() {
   var total = parseInt(agility) + parseInt(force) + parseInt(endurance);
 
   document.getElementById("total").innerHTML = total;
+  if(total !=100) {
+    document.querySelector("#total1").innerHTML = "La somme des 3 caractéristiques ne fait pas 100";
+  } else {
+    document.querySelector("#total1").innerHTML = "Héros validé";
+  }
 }
